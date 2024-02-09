7c56acbb6bcfcc570cc5cc0332141b5990551e2f
Miretskiy Vladislav <ipz221_mvya@student.ztu.edu.ua>
2024-02-09 20:30:02 +0200

    Create ConsoleAPP

---
+++ PracticaTask5/PracticaTask5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticaTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть число а:");
+            var a = float.Parse(Console.ReadLine()); 
+            Console.WriteLine("Введiть число b:");
+            var b = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Сумма дорiвнює: {a + b}");
+        }
+    }
+}
